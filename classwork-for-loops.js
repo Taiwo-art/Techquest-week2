d81bca3daae2a086f7a53b8a738e789512e29e3a
Taiwo Lambo <lmb.taiwo@gmail.com>
2022-08-26 14:39:52 +0100

    My first commit

---
+++ classwork-for-loops.js
@@ -0,0 +1,47 @@
+let age = [12,23,15,16,17,20,21,25,33,40,11,18];
+console.log(age.length)
+for(let m=0; m<age.length; m++) {
+console.log(age[m])
+
+let result = age[m]
+console.log(age[m])
+
+if (result>=20){
+    console.log(age[m])
+}
+if (result >= 10 && result <= 20){
+    console.log(age[m])
+}
+}
+
+
+let cars = [ "Honda", "Benz", "Mustang", "Bugati"];
+let carlength = cars.length
+console.log(cars.length)
+for(let q=1; q<carlength; q++);
+ console.log(`${cars(i)} !`)
+
+ //loops---counting from 1 -10----//
+for(let i=1;i<=10;i++){
+    console.log(i)
+}
+
+for (let j=0;j<=20;j+=2){
+    console.log(j)
+}
+
+//looping through an array
+
+let cars = [ "Honda", "Benz", "Mustang",  "Bugati"];
+let carlength = cars.length;
+console.log(carlength)
+for(let q=0; q<carlength; q++)
+ console.log(`${cars[q]}!`)
+
+ //tasks
+
+ let age = [12,23,15,16,17,20,21,25,33,40,11,18];
+ let agelength = age.length;
+ console.log(agelength)
+for(let m=0; m<agelength; m++;)
+console.log(age[m])
